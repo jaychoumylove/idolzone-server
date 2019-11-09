ab1ef26531b364592424c7204520d6bc96948974
LogiCadi <39653728+LogiCadi@users.noreply.github.com>
2019-11-09 22:54:35 +0800

    f

--- application/api/controller/v1/Notify.php
+++ application/api/controller/v1/Notify.php
@@ -34,7 +34,9 @@ class Notify extends Base
                     $content = UserExt::redress($user_id);
                     $content .= '<a data-miniprogram-appid=\"wx3a69eb5e1b2a7fa9\" data-miniprogram-path=\"/pages/user/log\" href=\"http://mp.weixin.qq.com/s?__biz=Mzg3MjAwODQ0Mw==&mid=2247483657&idx=1&sn=f6fed458fdb14f16f8a0e035b874a462&chksm=cef49e9df983178b8fc49143703041fa2b1c230da13d4154b11403d1cd215f38892c16f3a0be#rd\">点击此链接去查看</a>';
                 }
-            } 
+            } else if ($msg['Content'] == '充值' || $msg['Content'] == 1) {
+                $content .= '<a href=>充值</a>';
+            }
         }
 
         $wxMsg->autoSend($msg, 'text', [
@@ -42,5 +44,25 @@ class Notify extends Base
         ]);
 
         die('success');
+    }
+
+    public function createMenu()
+    {
+        $data = '{
+            "button":[
+            {
+            "type": "miniprogram",
+            "name": "打榜",
+            "url": "https://mp.weixin.qq.com/s/V-Zw-FDPKLKY4GJfBdZS7w",
+            "appid": "wx3a69eb5e1b2a7fa9",
+            "pagepath": "/pages/index/index"
+            },{
+            "type": "view",
+            "name": "充值",
+            "url": "https://idolzone.cyoor.com"
+            }]
+            }';
+
+        dump((new WxAPI('gzh'))->createMenu($data));
     }
 }
--- application/base/service/WxAPI.php
+++ application/base/service/WxAPI.php
@@ -271,4 +271,25 @@ class WxAPI
 
         return $this->request($url, $data);
     }
+
+    /**
+     * 创建菜单(认证后的订阅号可用)
+     *
+     * @param array $data
+     *            type可以选择为以下几种，其中5-8除了收到菜单事件以外，还会单独收到对应类型的信息。
+     *            1、click：点击推事件
+     *            2、view：跳转URL
+     *            3、scancode_push：扫码推事件
+     *            4、scancode_waitmsg：扫码推事件且弹出“消息接收中”提示框
+     *            5、pic_sysphoto：弹出系统拍照发图
+     *            6、pic_photo_or_album：弹出拍照或者相册发图
+     *            7、pic_weixin：弹出微信相册发图器
+     *            8、location_select：弹出地理位置选择器
+     */
+    public function createMenu($data)
+    {
+        $url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.  $this->appinfo['access_token'];
+        return $this->request($url, $data);
+        
+    }
 }
