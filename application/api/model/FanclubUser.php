5bef085cd1299ffca7753affa6090ac81e025fff
pifire <104460712@qq.com>
2020-01-26 22:39:26 +0800

    edit

--- application/api/model/FanclubUser.php
+++ application/api/model/FanclubUser.php
@@ -3,7 +3,7 @@
 namespace app\api\model;
 
 use app\base\model\Base;
-use think\Model;
+use think\Db;
 
 class FanclubUser extends Base
 {
@@ -23,5 +23,26 @@ class FanclubUser extends Base
         $fid = self::where('user_id', $uid)->value('fanclub_id');
         $f_user = Fanclub::where('id', $fid)->value('user_id');
         return $f_user == $uid;
+    }
+    
+    /**我在圈子里的排名信息 */
+    public static function getMyRankInfo($uid, $fid, $field)
+    {
+        $where = $fid ? ['user_id' => $uid, 'fanclub_id' => $fid] : ['user_id' => $uid];
+        $res = self::where($where)->field('user_id,'.$field)->find();
+        
+        $res['score'] = $res[$field];
+        
+        if ($res['score']) {
+             $where = $fid ? ['fanclub_id' => $fid] : '1=1';
+             $res['rank'] = self::where($where)->where($field, '>', $res['score'])->count() + 1;
+        } else {
+            $res['rank'] = '未上榜';
+        }        
+        $res['hasExited'] = Db::name('fanclub_user')->where('user_id', $uid)->where('delete_time','NOT NULL')->value('delete_time');
+            
+        // 等级
+        $res['level'] = CfgUserLevel::getLevel($uid);
+        return $res;
     }
 }
