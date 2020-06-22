8bd4c46b08a6f598d78f62ff3a2115a63ab13af8
aladin <104460712@qq.com>
2020-06-22 19:01:45 +0800

    福利修改成前10名

--- application/api/model/CfgPkactive.php
+++ application/api/model/CfgPkactive.php
@@ -13,7 +13,7 @@ class CfgPkactive extends Base
         //活动是不是进行中
         if (Cfg::getStatus('pkactive_date')){
 
-            $res = PkUserRank::where('last_pk_time',$pkTime)->order('last_pk_count desc,update_time asc')->limit(5)->field('id,mid')->select();
+            $res = PkUserRank::where('last_pk_time',$pkTime)->order('last_pk_count desc,update_time asc')->limit(10)->field('id,mid')->select();
             foreach ($res as $key=>$value){
                 //查询对应的奖励
                 $score = self::where('rank',$key+1)->value('score');
