65d6eaa5b91d8ec5f1edd12a95b2608cd143e38c
caixian <qq52577517@163.com>
2020-09-08 11:05:53 +0800

    解锁专属宠物碎片

--- backup/mysql.sql
+++ backup/mysql.sql
@@ -1241,5 +1241,8 @@ alter table f_rec_user_background_task
 alter table f_fanclub_user
 	add day_mass_times int default 0 not null comment '每日集结次数';
 
+alter table f_cfg_scrap modify type varchar(159) default 'WEEK' not null comment '类型
+day 每日碎片
+week 每周碎片';
 
 
