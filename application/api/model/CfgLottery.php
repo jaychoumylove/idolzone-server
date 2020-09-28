4cd741962cc09a889fc85a55b2a960528259c472
caixian <qq52577517@163.com>
2020-09-28 17:57:55 +0800

    中秋抽奖双倍取消

--- application/api/model/CfgLottery.php
+++ application/api/model/CfgLottery.php
@@ -11,23 +11,9 @@ class CfgLottery extends Base
     public static function doubleAward()
     {
         // 567
-        $status = Cfg::checkConfigTime(Cfg::MANOR_NATIONAL_DAY);
-        if ($status) {
-            self::where('1=1')->update([
-                'num' => Db::raw('weekend')
-            ]);
-        } else {
-            $week = date('w');
-            $double = [5,6,0];
-            if (in_array($week, $double)) {
-                self::where('1=1')->update([
-                    'num' => Db::raw('weekend')
-                ]);
-            } else {
-                self::where('1=1')->update([
-                    'num' => Db::raw('normal')
-                ]);
-            }
-        }
+        $week = date('w');
+        $double = [5,6,0];
+        $type = in_array($week, $double) ? 'weekend': 'normal';
+        self::where('1=1')->update(['num' => Db::raw($type)]);
     }
 }
